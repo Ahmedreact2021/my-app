65b6a9069663e9b78e1850f1a6b57ae63c4779f7
Ahmedreact2021 <81281535+Ahmedreact2021@users.noreply.github.com>
2021-05-24 00:07:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {LoginFormC, LoginFormF} from './LoginForm';
+
+
+
+
+
+
+
+function App() {
+
+  const [visisble, setVisible] = useState(true);
+
+  return (
+    <div>
+      {visisble ? <>
+        <LoginFormC />
+        <LoginFormF />
+      </> : null}
+
+      <button onClick={() => setVisible(! visisble)}>Toggle</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/App2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ClassComonent from './Components/ClassComponent';
+import FunctionComponent from './Components/FunctionComponent';
+
+function App2() {
+  return (
+    <div>
+      <ClassComonent />
+      <hr />
+      <FunctionComponent />
+
+    </div>
+  );
+}
+
+export default App2;
